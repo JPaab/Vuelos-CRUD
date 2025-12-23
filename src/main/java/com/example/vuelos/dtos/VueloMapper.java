be79d11e9c25523adf306192daaee4c37a00a44e
JPab <pabl0vargaas@hotmail.com>
2025-12-23 17:17:10 +0100

    Añadida la logica del VueloMapper.

--- src/main/java/com/example/vuelos/dtos/VueloMapper.java
+++ src/main/java/com/example/vuelos/dtos/VueloMapper.java
@@ -1,4 +1,33 @@
 package com.example.vuelos.dtos;
 
+import com.example.vuelos.models.Vuelo;
+
+import java.time.temporal.ChronoUnit;
+
 public class VueloMapper {
+
+    public static Vuelo toModel(VueloRequestDTO dto) {
+        Vuelo v = new Vuelo();
+        v.setNombreVuelo(dto.getNombreVuelo());
+        v.setEmpresa(dto.getEmpresa());
+        v.setLugarSalida(dto.getLugarSalida());
+        v.setLugarLlegada(dto.getLugarLlegada());
+        v.setFechaSalida(dto.getFechaSalida());
+        v.setFechaLlegada(dto.getFechaLlegada());
+        return v;
+    }
+
+    public static VueloResponseDTO toDTO(Vuelo vuelo) {
+        long duracion = ChronoUnit.DAYS.between(vuelo.getFechaSalida(), vuelo.getFechaLlegada());
+        return new VueloResponseDTO(
+                vuelo.getId(),
+                vuelo.getNombreVuelo(),
+                vuelo.getEmpresa(),
+                vuelo.getLugarSalida(),
+                vuelo.getLugarLlegada(),
+                vuelo.getFechaSalida(),
+                vuelo.getFechaLlegada(),
+                duracion
+        );
+    }
 }
