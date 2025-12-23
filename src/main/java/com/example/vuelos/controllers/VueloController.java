18af943daf4085c9070706985e3475750ff0e974
JPab <pabl0vargaas@hotmail.com>
2025-12-23 21:18:19 +0100

    Terminada la logica del VueloController, añadido el GetMapping que faltaba para listar por ID, el PostMapping para crear un vuelo, el PutMapping para cambiar los datos apartir del ID de un vuelo y el DeleteMapping para eliminar un vuelo.

--- src/main/java/com/example/vuelos/controllers/VueloController.java
+++ src/main/java/com/example/vuelos/controllers/VueloController.java
@@ -1,16 +1,16 @@
 package com.example.vuelos.controllers;
 
 import com.example.vuelos.dtos.VueloMapper;
+import com.example.vuelos.dtos.VueloRequestDTO;
 import com.example.vuelos.dtos.VueloResponseDTO;
 import com.example.vuelos.models.ApiResponse;
+import com.example.vuelos.models.Vuelo;
 import com.example.vuelos.services.VueloService;
 import com.example.vuelos.utils.DateUtils;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -43,5 +43,33 @@ public class VueloController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new ApiResponse<>(true, "Listado de vuelos", data));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<VueloResponseDTO>> obtenerPorId(@PathVariable int id) {
+        Vuelo v = service.obtenerPorId(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(true, "Vuelo encontrado", VueloMapper.toDTO(v)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<VueloResponseDTO>> crear(@Valid @RequestBody VueloRequestDTO dto) {
+        Vuelo creado = service.crear(VueloMapper.toModel(dto));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(true, "Vuelo creado", VueloMapper.toDTO(creado)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<VueloResponseDTO>> actualizar(@PathVariable int id, @Valid @RequestBody VueloRequestDTO dto) {
+        Vuelo actualizado = service.actualizar(id, VueloMapper.toModel(dto));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(true, "Vuelo actualizado correctamente", VueloMapper.toDTO(actualizado)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<VueloResponseDTO>> eliminar(@PathVariable int id) {
+        service.eliminar(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(true, "Vuelo eliminado correctamente", null));
     }
 }
